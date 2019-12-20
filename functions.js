6bc9b75735e4ff40ebd2a72dd8ee4b59c6d43106
Tuhin Mandal <tuhinmandal256@gmail.com>
2019-12-20 19:38:33 +0530

    function expression example

--- functions.js
+++ functions.js
@@ -12,6 +12,39 @@ console.log("my age is = ", myAge)
 function yearsUntillRetirement(year, firstName) {
     var age = calculateAge(year)
     timeRemainingToRetire = 60 - age
-    console.log(firstName + " retires in " + timeRemainingToRetire + " years")
+    if (timeRemainingToRetire > 0) {
+        console.log(firstName + " retires in " + timeRemainingToRetire + " years")
+    } else {
+        console.log(firstName + " is already retired")
+    }
+   
 }
 yearsUntillRetirement(1986, "Ps")
+yearsUntillRetirement(1985, "PP")
+yearsUntillRetirement(1980, "James")
+yearsUntillRetirement(1960, "Bond")
+yearsUntillRetirement(1950, "pa")
+/**
+ * Function statements and Expressins 
+ */
+
+
+// this is a function expression
+ var whatDoUDo = function(job, firstName) {
+
+    switch(job) {
+        case 'teacher':
+            return firstName + " teaches kids how to code"
+        case 'designer':
+            return firstName + " creates fancy clothes"
+        case 'driver':
+            return firstName + " drives a cab in lisbon"
+        default:
+            return firstName + " does something else"
+    }
+ }
+
+console.log( whatDoUDo("teacher", "john"))
+console.log( whatDoUDo("designer", "jane"))
+console.log( whatDoUDo("driver", "Mark"))
+console.log( whatDoUDo("retire", "Paul"))
