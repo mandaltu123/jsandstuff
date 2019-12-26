eb9abd255a74c3ef5133e040b3b60010c7934ed0
tuhin_mandal <tuhinmandal256@gmail.com>
2019-12-26 14:10:50 +0530

    adding example of a function returning a function

--- oops-in-js/first_class_functions.js
+++ oops-in-js/first_class_functions.js
@@ -48,3 +48,30 @@ let rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+console.log("----------------------------- THIS SECTION FOR FUNCTION RETURNING --------------------------------------")
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is ?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + ' ?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
+    }
+}
+
+let teacherQuestion = interviewQuestion('teacher');
+let designerQuestion = interviewQuestion('designer');
+let generalQuestion = interviewQuestion('f1 driver');
+
+teacherQuestion('john');
+designerQuestion('Jenny');
+generalQuestion('lewis');
+
+interviewQuestion('teacher')('Nita Strauss');
